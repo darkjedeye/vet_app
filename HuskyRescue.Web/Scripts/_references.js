fbd22492ad363b8d7cd0aa3a18f58cc3ed703c40
darkjedeye <eric@kleincore.co.za>
2017-12-11 05:51:14 +0200

    adding new repo

--- HuskyRescue.Web/Scripts/_references.js
+++ HuskyRescue.Web/Scripts/_references.js
@@ -1,4 +1,5 @@
-﻿/// <reference path="jquery-1.11.3.js" />
+﻿/// <reference path="modernizr-2.7.2.js" />
+/// <reference path="jquery-ui-1.10.4.js" />
+/// <reference path="jquery-1.11.0.js" />
 /// <reference path="Vendor/jQuery/jquery-migrate-1.2.1.js" />
 /// <reference path="Vendor/jQuery/UI/jquery.ui.totop.js" />
-/// <reference path="Vendor/Misc/modernizr-2.7.2.js" />
