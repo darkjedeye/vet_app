dfa5aeda2292e81d751df87be1914b91bc119f45
eric@wealthport.co.za <Mundane101!>
2017-12-13 17:33:43 +0200

    Modif]ied the files with dave to get it in a semi working order....:

--- HuskyRescue.Web/Scripts/_references.js
+++ HuskyRescue.Web/Scripts/_references.js
@@ -1,5 +1,4 @@
-﻿/// <reference path="modernizr-2.7.2.js" />
-/// <reference path="jquery-ui-1.10.4.js" />
-/// <reference path="jquery-1.11.0.js" />
+﻿/// <reference path="Vendor/jQuery/jquery-1.11.0.js" />
 /// <reference path="Vendor/jQuery/jquery-migrate-1.2.1.js" />
 /// <reference path="Vendor/jQuery/UI/jquery.ui.totop.js" />
+/// <reference path="Vendor/Misc/modernizr-2.7.2.js" />
